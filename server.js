ee1d10c3ada987594baf6927e85674c42d3d40c8
Tleds <>
2021-04-12 15:22:27 +0000

    change https server

--- server.js
+++ server.js
@@ -2,24 +2,27 @@ const express = require('express')
 const path = require('path')
 const fs = require('fs')
 const https = require('https')
+const cors = require('cors');
 
 const httpPort = 3000
-const httpsPort = 30001
-const key = fs.readFileSync('./certs/localhost.key');
-const cert = fs.readFileSync('./certs/localhost.crt');
+// const httpsPort = 30001
+// const key = fs.readFileSync('./certs/localhost.key');
+// const cert = fs.readFileSync('./certs/localhost.crt');
 
 const app = express()
-const server = https.createServer({key: key, cert: cert }, app);
+// const server = https.createServer({key: key, cert: cert }, app);
 
-app.use (function (req, res, next) {
-  if (req.secure) {
-          // request was via https, so do no special handling
-          next();
-  } else {
-          // request was via http, so redirect to https
-          res.redirect('https://' + req.headers.host + '1' + req.url);
-  }
-});
+app.use(cors(''));
+
+// app.use (function (req, res, next) {
+//   if (req.secure) {
+//           // request was via https, so do no special handling
+//           next();
+//   } else {
+//           // request was via http, so redirect to https
+//           res.redirect('https://' + req.headers.host + req.url);
+//   }
+// });
 
 app.use(express.static(path.join(__dirname, 'public')))
 
@@ -31,6 +34,6 @@ app.listen(httpPort, function () {
   console.log(`Listening on port ${httpPort}!`)
 })
 
-server.listen(httpsPort, function () {
-    console.log(`Listening on port ${httpsPort}!`)
-})
+// server.listen(httpsPort, function () {
+//     console.log(`Listening on port ${httpsPort}!`)
+// })
